9f2cf0efe75b8e0261b275349057a7d7b40dea4e
baba-s <shotababa1206@gmail.com>
2022-09-12 20:12:27 +0900

    add

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# Kogane.UniTaskNextFrameExtensionMethods
+# Kogane UniTask Next Frame Extension Methods
+
+UniTask.NextFrame を GameObject 型や Component 型の拡張メソッドで呼び出せるようにするパッケージ
+
+## 使用例
+
+```csharp
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using Kogane;
+using UnityEngine;
+
+public class Example : MonoBehaviour
+{
+    private readonly CancellationTokenSource m_cancellationTokenSource = new();
+
+    private async UniTaskVoid Start()
+    {
+        await gameObject.NextFrame();
+        await gameObject.NextFrame( m_cancellationTokenSource.Token );
+        await this.NextFrame();
+        await this.NextFrame( m_cancellationTokenSource.Token );
+    }
+}
+```
---
+++ Runtime/UniTaskNextFrameExtensionMethods.cs
@@ -0,0 +1,43 @@
+﻿using System.Threading;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+
+namespace Kogane
+{
+    public static class UniTaskNextFrameExtensionMethods
+    {
+        public static async UniTask NextFrame( this GameObject self )
+        {
+            await UniTask.NextFrame( self.GetCancellationTokenOnDestroy() );
+        }
+
+        public static async UniTask NextFrame( this Component self )
+        {
+            await self.gameObject.NextFrame();
+        }
+
+        public static async UniTask NextFrame
+        (
+            this GameObject   self,
+            CancellationToken cancellationToken
+        )
+        {
+            var cancellationTokenSource = CancellationTokenSource.CreateLinkedTokenSource
+            (
+                cancellationToken,
+                self.GetCancellationTokenOnDestroy()
+            );
+
+            await UniTask.NextFrame( cancellationTokenSource.Token );
+        }
+
+        public static async UniTask NextFrame
+        (
+            this Component    self,
+            CancellationToken cancellationToken
+        )
+        {
+            await self.gameObject.NextFrame( cancellationToken );
+        }
+    }
+}
